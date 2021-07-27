62431fee743ef77f07bbbd9bd1c3f608f0e52c34
Sofia Scotti <sofiscotti@hotmail.com>
2021-07-27 20:03:25 -0300

    Form Contacto

---
+++ enviar.php
@@ -0,0 +1,12 @@
+<?php
+$destino= "doblel.mayorista@gmail.com"
+$Nombre= $_Post ["Nombre"];
+$Apellido= $_Post ["Apellido"];
+$Mensaje= $_Post ["Mensaje"];
+
+$contenido= "Nombre: " . $Nombre . "/nApellido: " . $Apellido . "/nMensaje: " . $Mensaje
+
+mail($destino,"Contacto", $contenido);
+
+header("Location:Gracias.html");
+
