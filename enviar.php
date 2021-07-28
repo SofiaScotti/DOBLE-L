db6fe85e9f81aa9e1710c19167ad47a2db1dbcf3
Sofia Scotti <sofiscotti@hotmail.com>
2021-07-27 22:18:08 -0300

    probando php

--- enviar.php
+++ enviar.php
@@ -1,14 +1,14 @@
 <?php
 $destino= "doblel.mayorista@gmail.com"
-$Nombre= $_Post ["Nombre"];
-$Apellido= $_Post ["Apellido"];
-$Mensaje= $_Post ["Mensaje"];
+$Nombre= $_POST ["Nombre"];
+$Apellido= $_POST ["Apellido"];
+$Mensaje= $_POST ["Mensaje"];
 
 $contenido= "Nombre: " . $Nombre . "/nApellido: " . $Apellido . "/nMensaje: " . $Mensaje
 
 mail($destino,"Contacto", $contenido);
 
-header(Location:"Gracias.html");
+header("Location:Gracias.html");
 
 ?>
 
