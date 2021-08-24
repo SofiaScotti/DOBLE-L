a4689824ded06969b89add47bdfa106319e23a17
Sofia Scotti <sofiscotti@hotmail.com>
2021-08-24 13:30:52 -0300

    1ra Entrega Proyecto Final JS

--- js/main.js
+++ js/main.js
@@ -1,26 +1,8 @@
-// class Product {
-//   constructor(marca, peso, precio) {
-//     this.marca = marca || undefined;
-//     this.peso = peso || undefined;
-//     this.precio = precio || undefined;
-//   }
-// }
+let user = prompt("Ingresá tu usuario");
+let password = prompt("Ingresá tu contraseña");
 
-// const Product = {
-//   name: "queso",
-//   price: 450,
-//   stock: 500,
-// };
-// console.log(Product);
-
-// function Product(brand, name, price, stock) {
-//   this.brand = brand;
-//   this.name = name;
-//   this.price = price;
-//   this.stock = stock;
-// }
-
-// ---------------------------- Desafío Clase 5 "Objetos  ------------------------------- //
+alert("¡Hola, " + user + "! Ya podés empezar tu compra.");
+console.log("El usuario " + user + " ha iniciado sesión.");
 
 class Product {
   constructor(brand, name, price, stock) {
@@ -29,15 +11,51 @@ class Product {
     this.price = price;
     this.stock = stock;
   }
-
-  checkStock(stock) {
-    prompt("Stock Disponible: " + stock + " ¿Cuántos querés comprar?");
-  }
 }
 
-const product = new Product("La Paulina", "Queso Cremoso", 450, 800);
-const product2 = new Product("La Paulina", "Queso Roquefort", 630, 500);
-const product3 = new Product("Paladini", "Jamón Cocido", 400, 300);
+const product1 = new Product("La Paulina", "Queso Cremoso", 396, 800);
+const product2 = new Product(
+  "La Paulina",
+  "Queso Cremoso Doble Crema",
+  405,
+  500
+);
+const product3 = new Product("La Paulina", "Queso Dambo", 320, 300);
+const product4 = new Product("La Paulina", "Queso Goya", 458, 300);
+const product5 = new Product("La Paulina", "Queso Pategrás", 360, 750);
+const product6 = new Product("La Paulina", "Queso Azul", 473, 345);
+const product7 = new Product("Calchaquí", "Jamón Cocido Natural", 205, 600);
+const product8 = new Product("Cagnoli", "Salame Fuet", 112, 720);
+const product9 = new Product(
+  "Krachitos",
+  "Papas Fritas Corte Americano",
+  234,
+  950
+);
+const product10 = new Product(
+  "Krachitos",
+  "Papas fritas Corte Tradicional",
+  229,
+  950
+);
+const product11 = new Product("Krachitos", "Bastonitos", 255, 950);
 
-product.checkStock(200);
-console.log(product, product2, product3);
+const dataBase = [
+  product1,
+  product2,
+  product3,
+  product4,
+  product5,
+  product6,
+  product7,
+  product8,
+  product9,
+  product10,
+  product11,
+];
+
+console.log(dataBase.length);
+
+dataBase.forEach((product) => {
+  console.log(product);
+});
