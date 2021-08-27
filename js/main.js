85c1ed2381afbc19c37c4c478bd87f9b1a95ccfe
Sofia Scotti <sofiscotti@hotmail.com>
2021-08-27 14:43:09 -0300

    method

--- js/main.js
+++ js/main.js
@@ -59,3 +59,45 @@ console.log(dataBase.length);
 dataBase.forEach((product) => {
   console.log(product);
 });
+
+function sumNumbers() {
+  const numberA = prompt("Ingresá cantidad de productos A");
+  const numberB = prompt("Ingresá cantidad de productos B");
+  const numberC = prompt("Ingresá cantidad de productos C");
+  console.log(numberA, numberB, numberC);
+}
+
+function firstNumber() {
+  const numberA = parseInt(prompt("Ingresá cantidad de productos A"));
+  return numberA;
+}
+
+function secondNumber() {
+  const numberB = parseInt(prompt("Ingresá cantidad de productos A"));
+  return numberB;
+}
+
+function thirdNumber() {
+  const numberC = parseInt(prompt("Ingresá cantidad de productos A"));
+  return numberC;
+}
+
+let numberA = parseInt(prompt("Ingresá cantidad de items A"));
+console.log("El usuario " + user + " compró " + numberA + " productos A");
+
+let numberB = parseInt(prompt("Ingresá cantidad de productos B"));
+console.log("El usuario " + user + " compró " + numberB + " productos B");
+
+let numberC = parseInt(prompt("Ingresá cantidad de productos C"));
+console.log("El usuario " + user + " compró " + numberC + " productos C");
+
+let total = 0;
+
+function sumNumbers(numberA, numberB, numberC) {
+  total = numberA + numberB + numberC;
+}
+
+sumNumbers(numberA, numberB, numberC);
+alert("¡Compraste " + total + " productos!");
+
+console.log("El usuario " + user + " compró " + total + " productos en total");
