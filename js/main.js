10afa1df13176d1eab738d77d8a8b73ca27eae29
Sofia Scotti <sofiscotti@hotmail.com>
2021-09-06 10:48:18 -0300

    cards desde JS - Desafío DOM

--- js/main.js
+++ js/main.js
@@ -5,40 +5,95 @@
 // console.log("El usuario " + user + " ha iniciado sesión.");
 
 class Product {
-  constructor(brand, name, price, stock) {
+  constructor(brand, name, price, stock, imagen) {
     this.brand = brand;
     this.name = name;
     this.price = price;
     this.stock = stock;
+    this.imagen = imagen;
+  }
+  mostrarMarca() {
+    console.log(this.brand);
   }
 }
 
-const product1 = new Product("La Paulina", "Queso Cremoso", 396, 800);
+const product1 = new Product(
+  "La Paulina",
+  "Queso Cremoso",
+  396,
+  800,
+  "./Productos/prod1.JPEG"
+);
 const product2 = new Product(
   "La Paulina",
   "Queso Cremoso Doble Crema",
   405,
-  500
+  500,
+  "./Productos/prod2.JPEG"
 );
-const product3 = new Product("La Paulina", "Queso Dambo", 320, 300);
-const product4 = new Product("La Paulina", "Queso Goya", 458, 300);
-const product5 = new Product("La Paulina", "Queso Pategrás", 360, 750);
-const product6 = new Product("La Paulina", "Queso Azul", 473, 345);
-const product7 = new Product("Calchaquí", "Jamón Cocido Natural", 205, 600);
-const product8 = new Product("Cagnoli", "Salame Fuet", 112, 720);
+const product3 = new Product(
+  "La Paulina",
+  "Queso Dambo",
+  320,
+  300,
+  "./Productos/prod3.JPEG"
+);
+const product4 = new Product(
+  "La Paulina",
+  "Queso Goya",
+  458,
+  300,
+  "./Productos/prod4.JPEG"
+);
+const product5 = new Product(
+  "La Paulina",
+  "Queso Pategrás",
+  360,
+  750,
+  "./Productos/prod17.JPEG"
+);
+const product6 = new Product(
+  "La Paulina",
+  "Queso Azul",
+  473,
+  345,
+  "./Productos/prod16.JPEG"
+);
+const product7 = new Product(
+  "Calchaquí",
+  "Jamón Cocido Natural",
+  205,
+  600,
+  "./Productos/prod9.JPEG"
+);
+const product8 = new Product(
+  "Cagnoli",
+  "Salame Fuet",
+  112,
+  720,
+  "./Productos/prod11.JPEG"
+);
 const product9 = new Product(
   "Krachitos",
   "Papas Fritas Corte Americano",
   234,
-  950
+  950,
+  "./Productos/prod13.JPEG"
 );
 const product10 = new Product(
   "Krachitos",
   "Papas fritas Corte Tradicional",
   229,
-  950
+  950,
+  "./Productos/prod14.JPEG"
 );
-const product11 = new Product("Krachitos", "Bastonitos", 255, 950);
+const product11 = new Product(
+  "Krachitos",
+  "Bastonitos",
+  255,
+  950,
+  "./Productos/prod15.JPEG"
+);
 
 const dataBase = [
   product1,
@@ -53,24 +108,29 @@ const dataBase = [
   product10,
   product11,
 ];
+const dataBaseLength = dataBase.length;
+console.log(dataBaseLength);
+for (let i = 0; i < dataBaseLength; i++) {
+  dataBase[i].mostrarMarca();
+}
 
 let acumulador = ``;
-dataBase.forEach((Product) => {
-  <div class="container">
+dataBase.forEach((myProducts) => {
+  acumulador += `<div class="container">
     <div class="row NuestrosProductos__Card--Prod">
       <div class="col-12 col-md-4">
         <div class="card" style="width: 18rem">
           <img
-            src="./Productos/prod1.JPEG"
+            src="${myProducts.imagen}"
             class="card-img-top"
             alt="Queso Cremoso"
           />
           <div class="card-body">
-            <h5 class="card-title"> ${name}</h5>
-            <p class="card-text"> ${price} </p>
+            <h5 class="card-title"> ${myProducts.name}</h5>
+            <p class="card-text"> ${myProducts.price} </p>
             <a
               href="#"
-              onclick="agregarAlCarrito(${Product.name})"
+              onclick="agregarAlCarrito(${myProducts.name})"
               class="btn btn-primary"
             >
               Agregar al Carrito
@@ -79,13 +139,13 @@ dataBase.forEach((Product) => {
         </div>
       </div>
     </div>
-  </div>;
+  </div>;`;
 });
 
-document.getElementsByClassName("NuestrosProductos__Card--Prod").innerHTML =
-  acumulador;
+const contenedorCards = document.getElementById("contenedor-productos");
 
-console.log();
+contenedorCards.innerHTML = acumulador;
+console.log(contenedorCards);
 
 // function sumNumbers() {
 //   const numberA = prompt("Ingresá cantidad de productos A");
